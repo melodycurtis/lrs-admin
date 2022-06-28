e0256378e4ceb90c606e502e9dfb2f1e78f4d00e
melodycurtis <melodycurtis123@gmail.com>
2022-06-28 17:12:09 +0100

    change back to LRS

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Manage release on licence',
+  serviceName: 'Manage learner record applications',
 
   // Default port that prototype runs on
   port: '3000',
