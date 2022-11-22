080f156cbc65a37db5869a5045817a684b7319dc
melodycurtis <melodycurtis123@gmail.com>
2022-11-22 10:20:04 +0000

    nn

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Manage learner record applications',
+  serviceName: 'ESFA performance reports',
 
   // Default port that prototype runs on
   port: '3000',
